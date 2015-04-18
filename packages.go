db420ac0de8fef7ea768b6db124cc45e2d9d4cf0
vikashvikram <vikashvikram@gmail.com>
2015-04-19 02:35:07 +0530

    more examples of go language

--- packages.go
+++ packages.go
@@ -11,10 +11,10 @@ func main() {
 	fmt.Println("My favorite number is", rand.Intn(100))
 	fmt.Println("My favorite number is", rand.Intn(100))
 	s1 := rand.NewSource(42)
-  r1 := rand.New(s1)
-  fmt.Println("My favorite number is", s1)
-  fmt.Println("My favorite number is", r1.Intn(100))
-  fmt.Println("My favorite number is", r1.Intn(100))
-  fmt.Println("My favorite number is", r1.Intn(100))
+	r1 := rand.New(s1)
+	fmt.Println("My favorite number is", s1)
+	fmt.Println("My favorite number is", r1.Intn(100))
+	fmt.Println("My favorite number is", r1.Intn(100))
+	fmt.Println("My favorite number is", r1.Intn(100))
 }
 
